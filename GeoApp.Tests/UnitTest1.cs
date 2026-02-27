bd46be1e9a3003d6ff0c25f1a0062fa0db1e0ac5
arturopacocruz <arturopacocruz@gmail.com>
2026-02-27 13:34:50 -0400

    Guarda mis cambios antes del rebase

---
+++ GeoApp.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using Xunit;
+using GeoApp.Models;
+public class UnitTest1
+{
+ [Fact]
+ public void TestAreaRectangulo()
+ {
+ double resultado = Calculadora.AreaRectangulo(10, 5);
+ Assert.Equal(50, resultado);
+ }
+} 
